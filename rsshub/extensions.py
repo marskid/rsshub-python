51ec415e7bec068f9f1f83a8e61cd1afbbfe1d82
marskid <marskid@yeah.net>
2026-03-25 17:28:38 +0800

    update

--- rsshub/extensions.py
+++ rsshub/extensions.py
@@ -1,10 +1,10 @@
-from flask_bootstrap import Bootstrap4
+from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_caching import Cache
 import os
 
 
-bootstrap = Bootstrap4()
+bootstrap = Bootstrap()
 moment = Moment()
 
 # 仅在开发环境中导入和初始化 debugtoolbar
